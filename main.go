f27773238c167516893f1d4c1b767a422c6f2012
Ulf Dellbrügge <xintamosaik@gmail.com>
2026-03-14 15:35:25 +0100

    parse multiform

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-
+	"strings"
 	"strconv"
 
 	"time"
@@ -30,20 +30,29 @@ const html_test = `
 
 // helper to parse common form fields from a request
 func parseTodoForm(r *http.Request) (short string, description string, dueDate *time.Time, costOfDelay int16, effort string, err error) {
-	if err = r.ParseForm(); err != nil {
+	if strings.HasPrefix(r.Header.Get("Content-Type"), "multipart/form-data") {
+		err = r.ParseMultipartForm(1 << 20)
+	} else {
+		err = r.ParseForm()
+	}
+	if err != nil {
 		return
 	}
+
 	short = r.FormValue("short")
 	description = r.FormValue("description")
+
 	dateStr := r.FormValue("due_date")
 	if dateStr != "" {
 		var dt time.Time
 		dt, err = time.Parse("2006-01-02", dateStr)
 		if err != nil {
+			err = fmt.Errorf("due_date must be in YYYY-MM-DD format")
 			return
 		}
 		dueDate = &dt
 	}
+
 	if codStr := r.FormValue("cost_of_delay"); codStr != "" {
 		var tmp int
 		tmp, err = strconv.Atoi(codStr)
@@ -56,16 +65,17 @@ func parseTodoForm(r *http.Request) (short string, description string, dueDate *
 		}
 		costOfDelay = int16(tmp)
 	}
+
 	effort = r.FormValue("effort")
 	switch effort {
 	case "mins", "hours", "days", "weeks", "months":
-		// ok
 	case "":
 		effort = "hours"
 	default:
 		err = fmt.Errorf("invalid effort")
 		return
 	}
+
 	return
 }
 
--- todo.md
+++
@@ -1,2 +0,0 @@
-undefined: todosList
-undefined: todoForm
